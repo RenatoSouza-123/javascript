996faec285c1bd44f68b3b03c483479d1423f707
RenatoSouza-123 <renatosouza451@gmail.com>
2022-11-29 15:37:10 -0300

    Exercícios sobre funções

---
+++ aula16/funcao001.js
@@ -0,0 +1,9 @@
+function parImpar(num) {
+    if (num % 2 == 0) {
+        return console.log('O número é par')
+    } else {
+        return console.log('O número é ímpar')
+    }
+}
+
+parImpar(5435)
---
+++ aula16/funcao002.js
@@ -0,0 +1,5 @@
+function soma(n1 = 0, n2 = 0) { //caso não sejam passados, tanto n1 quanto n2 serão iguais a 0
+    return n1 + n2
+} 
+
+console.log(soma(2, 5))
