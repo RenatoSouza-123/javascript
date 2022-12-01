56a288dda07aad8646b10e3ba420801aaf328954
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-01 18:00:04 -0300

    Aula sobre booleans

---
+++ udemy_bootcamp/aula02/7-booleans_intro.js
@@ -0,0 +1,19 @@
+//TRUE or FALSE values
+
+console.log(1 == 0)
+console.log(false)
+
+//we can store boolean values in variables
+let isLoggedIn = true;
+
+let gameOver = false;
+
+const isWaterWet = true;
+
+let isHungry = true;
+console.log(isHungry)
+
+//Variables can change type
+let numDonuts = 12; //it's a number
+numDonuts = false; //now it's a boolean
+numDonuts = 5165436; //back to number
