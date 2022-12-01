70b0da1a50e962c954e0c7b7a5f6deebfbabf74b
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-01 18:07:38 -0300

    Aula sobre strings

---
+++ udemy_bootcamp/aula02/8-strings.js
@@ -0,0 +1,21 @@
+/*
+In JavaScript, strings are pieces of text, or strings of characters. 
+We wrap them in quotes.
+*/
+
+let firstName = "Ziggy";
+let msg = "Please do not feed the chimps";
+let animal = 'Dumbo Octopus'
+//let bad = "this is wrong' //This DOES NOT work
+ 
+//typeof operator
+console.log(typeof '87')
+console.log(typeof 87)
+
+console.log("he said, 'hahaha'") //this works
+
+let lastName = 'Stardust';
+
+console.log(firstName + ' ' + lastName)
+
+console.log("hi" + 1) //1 is going to be turned into a string
