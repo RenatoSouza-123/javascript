001a82a4138f96f68bfcc2b228fbffa91aa4e51d
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-01 17:42:38 -0300

    Aula sobre operadores unários

---
+++ udemy_bootcamp/aula01/4-unary_operators.js
@@ -0,0 +1,23 @@
+let score = 0;
+
+score = score + 10;
+
+//shorthand
+score += 1;
+console.log(score)
+
+score -= 100;
+score += 150;
+
+let bonusMult = 3;
+
+score *= bonusMult;
+console.log(score)
+
+let counter = 0;
+
+//unary operators
+counter++;
+console.log(counter)
+
+counter--;
