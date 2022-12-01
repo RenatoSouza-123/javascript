4f5ba4ecaef80d18a348da69ffa8e6b0bd11a12f
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-01 17:47:36 -0300

    aula sobre const

---
+++ udemy_bootcamp/aula01/5-const.js
@@ -0,0 +1,14 @@
+/*
+const works just like let, except you CANNOT change the value
+*/
+
+const year = 1973;
+year += 1; //ERROR
+
+//Why use const?
+const pi = 3.14159;
+const daysInWeek = 7;
+const minHeightForRide = 60;
+
+const peopleCount = 10;
+let peopleCount = 3; //ERROR
