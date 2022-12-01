63270ca23bd024198f076686f6095b74e0e6c9ff
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-01 17:24:22 -0300

    Primeiros passos com números

---
+++ udemy_bootcamp/numbers.js
@@ -0,0 +1,21 @@
+/*
+JavaScript has ONE Number type
+Positive numbers
+Negative numbers
+Whole numbers (integers)
+Decimal numbers
+*/
+
+//math operations
+console.log(50 + 5)
+console.log(50 - 5)
+console.log(50 / 5)
+console.log(50 * 5)
+console.log(50 % 5) //it gives the remainder of the division
+console.log(50 ** 5)
+
+/*
+Standard order of operations
+
+*/
+
