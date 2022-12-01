bcb7540208ffc751ede87e1e162941125343dc7e
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-01 17:51:51 -0300

    Aula sobre var

---
+++ udemy_bootcamp/aula01/6-var.js
@@ -0,0 +1,10 @@
+/*
+Before let & const, var was the only way of declaring variables. 
+These days, there isn't really a reason to use it.
+*/
+
+var eggs = 12;
+
+eggs++ //we can reset it, it is not like const
+console.log(eggs);
+
