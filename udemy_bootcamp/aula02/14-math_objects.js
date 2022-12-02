0f73e5dd412e726a4a3c347a60c90681d633f5fe
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-02 18:30:07 -0300

    Math objects and random numbers

---
+++ udemy_bootcamp/aula02/14-math_objects.js
@@ -0,0 +1,23 @@
+/*
+MATH OBJECTS
+Contain properties and methods for mathematical constants and functions
+*/
+
+console.log(Math.PI)
+
+console.log(Math.floor(3.657))
+
+console.log(Math.round(4.7))
+
+console.log(Math.pow(9, 2))
+
+/*
+Random Numbers
+Math.random() gives us a random decimal between 0 and 1 (non-inclusive)
+*/
+
+console.log(Math.random())
+
+//random numbers from 1 to 10
+
+console.log(Math.floor(Math.random() * 10) + 1)
