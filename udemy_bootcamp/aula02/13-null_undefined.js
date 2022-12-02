c0d706a04bfc098117b0a8d335417b765f748d43
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-02 18:22:08 -0300

    Null & Undefined

---
+++ udemy_bootcamp/aula02/13-null_undefined.js
@@ -0,0 +1,13 @@
+/*
+Null:
+    - Intentional absence of any value
+    - Must be assigned
+Undefined:
+    - Variables that do not have an assigned value are undefined
+*/
+
+//No one is logged in yet
+let loggedInUser = null; //value is explicitly nothing
+
+//A user logs in
+loggedInUser = 'Alan Rickman';
