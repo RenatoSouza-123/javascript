5caaf31a3e0aafc6c7a1e4603823a4263a9b784a
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-02 13:12:34 -0300

    String escapes

---
+++ udemy_bootcamp/aula02/11-string_escape_characters.js
@@ -0,0 +1,12 @@
+/*
+\n - new line
+\' - single quote
+\" - double quote
+\\ - backslash
+\t - tab
+*/
+
+console.log('he said I ain\'t happy')
+console.log("he said \"I ain't happy\"")
+console.log("Hello\nGoodbye")
+console.log('\\t means a tab in the sentence')
