1cbafd309f92b9986d8f409c287bedf902f9ce8a
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-02 09:09:40 -0300

    Aula sobre strings

---
+++ udemy_bootcamp/aula02/9-string-indices.js
@@ -0,0 +1,13 @@
+//Strings are indexed
+//Each character has a corresponding index (a positional number)
+
+console.log('hello'.length)
+console.log('hello   '.length)
+
+let mySong = "Surfin' USA"
+console.log(mySong.length)
+console.log(mySong[0])
+console.log(mySong[10])
+console.log(mySong[mySong.length - 1])
+
+//strings are immutable in JavaScript
