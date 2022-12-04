45de188f31a540df7ba3de19b9f70d9f5a696513
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-04 11:55:42 -0300

    Triple equals

---
+++ udemy_bootcamp/aula03/19-triple_equals.js
@@ -0,0 +1,20 @@
+//Triple equals check for equality of value AND type
+
+console.log(2 == '2') // true
+console.log(2 === '2') //false
+
+console.log(0 == false) // true
+console.log(0 === false) // false
+
+console.log(null == undefined) //true
+console.log(null === undefined) //false
+
+let isLoggedIn = false;
+console.log(isLoggedIn == false)
+
+//GO WITH TRIPLE EQUALS
+
+//Non-equality
+
+console.log(2 != '2') // false
+console.log(2 !== '2') // true
