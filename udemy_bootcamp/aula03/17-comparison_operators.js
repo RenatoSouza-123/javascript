16a4ed08a1e11682f22436dc43b8371056f52be1
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-04 11:45:32 -0300

    Comparison operators

---
+++ udemy_bootcamp/aula03/17-comparison_operators.js
@@ -0,0 +1,20 @@
+/*
+>   - greater than
+<   - less than
+>=  - greater than or equal to
+<=  - less than or equal to
+==  - equality
+!=  - not equal
+=== - strict equality
+!== - strict non-equality
+*/
+
+console.log(-2 > 1)
+console.log(-2 < 1)
+console.log(2 >= 2)
+console.log(3 <= 2)
+
+console.log('hello'.length > 'hello!'.length)
+console.log('a' < 'A')
+
+//search for list of unicode characters
