1e1a6159cfd806d5a95ca3025ad122756fd2d786
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-05 11:02:11 -0300

    Else if

---
+++ udemy_bootcamp/aula03/22-else_if.js
@@ -0,0 +1,9 @@
+let rating = 1;
+
+if (rating === 3) {
+    console.log('Well done')
+} else if (rating === 2){
+    console.log('Meets Expectations')
+} else if (rating === 1) {
+    console.log('Needs improvement')
+}
