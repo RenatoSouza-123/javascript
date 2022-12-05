d1b58feeaf1b6b0cd9782bf3704709ca55a5b413
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-05 18:51:47 -0300

    Array indices

---
+++ udemy_bootcamp/aula04/33-array_indices.js
@@ -0,0 +1,12 @@
+//ARRAYS ARE INDEXED
+
+let colors = ["red", "orange", "blue", "black", "purple"]
+
+console.log(colors.length)
+console.log(colors[0])
+
+//to get the last one
+console.log(colors[colors.length - 1])
+
+//we get undefined if we try to access something that is not there
+console.log(colors[123])
