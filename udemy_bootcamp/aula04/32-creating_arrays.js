e79f8fc1bb06bb84d014eeaca97aca8b743d85c8
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-05 18:41:09 -0300

    Creating arrays

---
+++ udemy_bootcamp/aula04/32-creating_arrays.js
@@ -0,0 +1,19 @@
+/*
+GOALS
+- Work with arrays
+- Write Object Literals
+- Understand Reference Types
+- Use common Array/Object methods
+*/
+
+//ARRAYS: ordered collections of values
+
+let shoppingList = ["cereal", "cheese", "ice"];
+console.log(shoppingList)
+
+let lotto = [45, 12, 34, 32, 54];
+
+let myCollection = [12, "dog", null, true, NaN];
+
+//a not very common way of initializing an array
+new Array(1, 2, 3, 4, 5)
