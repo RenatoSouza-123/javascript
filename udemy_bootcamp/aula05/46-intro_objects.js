7f07427a3708576ef7f255eeeed59dd79e26fb37
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-07 17:49:31 -0300

    Intro to objects

---
+++ udemy_bootcamp/aula05/46-intro_objects.js
@@ -0,0 +1,14 @@
+/*
+OBJECTS
+- Objects are collections of properties
+- Properties are a key-value pair
+- Rather than accessing data using an index, we use custom keys
+*/
+
+const fitBitData = {
+    totalSteps : 308727,
+    totalMiles: 211.7,
+    avgCalorieBurn: 5755,
+    workoutsThisWeek: '5 of 7',
+    avgGoodSleep: '2:13'
+}
