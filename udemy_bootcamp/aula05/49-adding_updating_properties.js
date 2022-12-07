c5182b62878e599b6bb747d8d12a95d6590b5191
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-07 20:12:57 -0300

    Adding and updating properties

---
+++ udemy_bootcamp/aula05/49-adding_updating_properties.js
@@ -0,0 +1,20 @@
+const userReviews = {};
+
+//adding values
+userReviews['queenBee49'] = 4.0;
+
+userReviews.mrSmith78 = 3.5;
+
+console.log(userReviews);
+
+//updating values
+userReviews.colt = '5';
+userReviews.colt = 5;
+
+console.log(userReviews);
+
+userReviews.queenBee49 += 2;
+console.log(userReviews)
+
+userReviews.mrSmith78++;
+console.log(userReviews)
