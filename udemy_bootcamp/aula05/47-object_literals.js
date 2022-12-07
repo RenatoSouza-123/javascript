72f1a665047f17eb40fee7e06f0e33521dde85b2
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-07 17:57:52 -0300

    Creating object literals

---
+++ udemy_bootcamp/aula05/47-object_literals.js
@@ -0,0 +1,14 @@
+const fitBitData = {
+    totalSteps       : 308727,
+    totalMiles       : 211.7,
+    avgCalorieBurn   : 5755,
+    workoutsThisWeek : '5 of 7',
+    avgGoodSleep     : '2:13'
+};
+
+console.log(fitBitData);
+
+//we can access information using the dot syntax
+console.log(fitBitData.totalMiles);
+
+//when we make an object, the keys are automatically converted to strings
