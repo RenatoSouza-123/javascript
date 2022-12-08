68991ce0c3b0428f3be31b0b5d33ba790c9c8cf3
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-08 12:00:05 -0300

    Intro to loops

---
+++ udemy_bootcamp/aula6/53-intro_to_loops.js
@@ -0,0 +1,20 @@
+/*
+GOALS
+- Write for loops
+- Write while loops
+- Avoid infinite loops
+- Iterate over arrays and objects
+
+LOOPS
+Doing things repeatedly
+- Loops allow us to repeat code
+    - Print "hello" 10 times
+    - Sum all numbers in an array
+- There are multiple types
+    - for loops
+    - while loop
+    - for...of loop
+    - for...in loop
+
+*/
+
