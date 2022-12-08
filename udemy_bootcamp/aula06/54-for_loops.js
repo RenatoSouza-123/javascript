120adb8a0c3a7bff7a77a5984d242cb08dcde54d
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-08 12:12:15 -0300

    For loops

---
+++ udemy_bootcamp/aula06/54-for_loops.js
@@ -0,0 +1,21 @@
+/*
+for (
+    [initialExpression];
+    [condition];
+    [incrementExpression]
+){
+
+}
+*/
+
+for (let i = 1; i <= 10; i++) {
+    console.log('Hello:', i)
+}
+
+for (num = 1; num <= 20; num++) {
+    console.log(`${num} x ${num} = ${num * num}`)
+}
+
+for (let i = 200; i >= 0; i -=25) {
+    console.log(i)
+}
