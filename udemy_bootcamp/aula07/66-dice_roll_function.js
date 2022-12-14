9de8c9ac9cfb009d8c5d071414e34b8415ee5eaa
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-14 16:20:28 -0300

    Roll dice function with math.random

---
+++ udemy_bootcamp/aula07/66-dice_roll_function.js
@@ -0,0 +1,12 @@
+function rollDie() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    console.log(`Rolled: ${roll}`)
+}
+
+function throwDice() {
+    for (let i = 1; i <= 6; i++){
+        rollDie();
+    }
+}
+
+throwDice();
