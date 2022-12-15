3323f46276639f33576a9bbfbb6bb2624e2fd34a
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-15 11:39:52 -0300

    Return statement

---
+++ udemy_bootcamp/aula07/69-return_statement.js
@@ -0,0 +1,13 @@
+/*
+Built-in methods return values when we call them. We can store those values
+
+const yell = "I will end you.".toUpperCase();
+console.log(yell)
+*/
+
+function sum(x, y) {
+    return x + y;
+}
+
+const add = sum(4, 7)
+console.log(add)
