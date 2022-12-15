1af9fa2c4c766f75d45075e4176cbccfb9aa83a3
RenatoSouza-123 <renatosouza451@gmail.com>
2022-12-15 11:32:58 -0300

    Functions with multiple args

---
+++ udemy_bootcamp/aula07/68-multiple_args.js
@@ -0,0 +1,18 @@
+function square(num) {
+    console.log(num * num)
+}
+
+square(5)
+
+function sum(x, y) {
+    console.log(x + y)
+}
+
+sum(5, 7)
+
+function divide(a, b) {
+    console.log(a / b)
+}
+
+divide(1, 4)
+divide(9, 7)
